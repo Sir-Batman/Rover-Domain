569b66aaf5b174fe4e19b80b9e5cd97315077211
Golden Rockefeller <rockefeg@oregonstate.edu>
2018-08-20 14:43:09 -0700

    Change main function to code in run file

--- run.py
+++ run.py
@@ -5,12 +5,11 @@ from simulation_mods import *
 # NOTE: Add the mod functions (variables) to run to modCol here:
 modCol = [globalRewardMod]
 
-def main():
-    i = 0
-    while True:
-        print("Run %i"%(i))
-        for mod in modCol:
-            sim = getSim()
-            mod(sim)
-            sim.run()
-        i += 1
+i = 0
+while True:
+    print("Run %i"%(i))
+    for mod in modCol:
+        sim = getSim()
+        mod(sim)
+        sim.run()
+    i += 1
